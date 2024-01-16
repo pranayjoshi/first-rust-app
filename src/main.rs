d5d22c6e3283a81a228836521ffbc03fdccf054c
pranayjoshi <pranayjoshi4466@gmail.com>
2024-01-16 16:13:49 -0600

    added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::fmt::format;
 
 slint::include_modules!();
 
@@ -11,14 +10,14 @@ fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
-    ui.divide_income(move |string| {
+    ui.on_divide_income(move |string| {
         let ui = ui_handle.unwrap();
         let num = string.parse::<f64>().unwrap();
         let tax = num * TAXPER;
         let owner = num * OWNERPER;
         let profit = num * PROFIT;
         let opex = num * OPEXPER;
-        let result = format!("args: {:.2}\ntax: {:.2}\nowner: {:.2}\nprofit: {:.2}\nopex: {:.2}", num, tax, owner, profit, opex);
+        let result = format!("Initial price: {:.2}\ntax: {:.2}\nowner: {:.2}\nprofit: {:.2}\nopex: {:.2}", num, tax, owner, profit, opex);
         ui.set_result(result.into());
     });
 
